edc37e50533db5e119c22df0f05b0211c699cc47
hackingyseguridad <antonio_taboada@hotmail.es>
2019-01-10 12:37:01 +0100

    Create generacert.sh

---
+++ generacert.sh
@@ -0,0 +1,12 @@
+
+# Instalación OpenSSL
+sudo apt-get install openssl
+
+# Generar formato PEM fichero con clave privada MyRootCA.key 
+openssl genrsa -out MyRootCA.key 2048
+
+# Generar a partir de la clave privada en MyRootCA el fichero con clave publica MyRootCA.pem
+openssl req -x509 -new -nodes -key MyRootCA.key -sha256 -days 1024 -out MyRootCA.pem
+
+# Convertir MyRootCA.pem en My
+openssl x509 -outform der -in MyRootCA.pem -out MyRootCA.crt
