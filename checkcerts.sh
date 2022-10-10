821a0440f635f7befa76440e6ad6e063fe2641cf
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-10 10:04:17 +0200

    Update checkcerts.sh

--- checkcerts.sh
+++ checkcerts.sh
@@ -1,4 +1,4 @@
-# chequea informacion de un listado de fqdn
-
 #!/bin/bash
+# (c) hacking y seguridad .com 2022
+# chequea informacion de un listado de fqdn
 for n in `cat fqdn.txt`; do echo $n; openssl s_client -connect $n |grep Verification; done
