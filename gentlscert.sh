4e8df36e5f90946381beb55138816a5cc524d96c
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-10 10:04:41 +0200

    Update gentlscert.sh

--- gentlscert.sh
+++ gentlscert.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
+# (c) hacking y seguridad .com 2022
 # Genera un autocertificado TLS
 openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out certificado.crt -keyout clave.key
